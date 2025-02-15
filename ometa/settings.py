e35a9c708d45c3ad92df7b2ae776fee587838d50
sergio11iw <sergio11iw@mail.ru>
2025-02-16 01:04:22 +0500

    регистрация

--- ometa/settings.py
+++ ometa/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'core',
+    'profiles',
 
 
 ]
@@ -134,3 +135,4 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+LOGIN_URL = '/profiles/login'
---
+++ profiles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('home', views.home, name='profiles_home'),
+    path('login', views.login, name='profiles_login'),
+    path('register', views.register, name='profiles_register'),
+    path('logout', views.logout, name='profiles_logout')
+]
